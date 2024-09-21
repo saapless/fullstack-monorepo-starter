c3813caa29002c78ec0bcc03863f4a139e6202c0
Silviu Glavan <silviu@gigs.ro>
2024-09-21 07:28:40 +0300

    chore: app build setup

---
+++ apps/web/serverless.ts
@@ -0,0 +1,123 @@
+import type { AWS } from "@serverless/typescript";
+
+export default {
+  // "org" ensures this Service is used with the correct Serverless Framework Access Key.
+  // org: "",
+  // "service" is the name of this project. This will also be added to your AWS resource names.
+  service: "web-app",
+  provider: {
+    name: "aws",
+    region: "${env:REGION, 'us-east-1'}" as AWS["provider"]["region"],
+    stage: "${env:STAGE, 'dev'}",
+  },
+  resources: {
+    Resources: {
+      AmplifyAppRole: {
+        Type: "AWS::IAM::Role",
+        Properties: {
+          AssumeRolePolicyDocument: {
+            Version: "2012-10-17",
+            Statement: [
+              {
+                Effect: "Allow",
+                Principal: {
+                  Service: ["amplify.amazonaws.com"],
+                },
+                Action: ["sts:AssumeRole"],
+              },
+            ],
+          },
+          Policies: [
+            {
+              PolicyName: "web-app-policy-${env:STAGE}",
+              PolicyDocument: {
+                Version: "2012-10-17",
+                Statement: [
+                  {
+                    Sid: "PushLogs",
+                    Effect: "Allow",
+                    Action: ["logs:CreateLogStream", "logs:PutLogEvents"],
+                    Resource: {
+                      "Fn::Sub":
+                        "arn:aws:logs:${AWS::Region}:${AWS::AccountId}:log-group:/aws/amplify/*:log-stream:*",
+                    },
+                  },
+                  {
+                    Sid: "CreateLogGroup",
+                    Effect: "Allow",
+                    Action: "logs:CreateLogGroup",
+                    Resource: {
+                      "Fn::Sub":
+                        "arn:aws:logs:${AWS::Region}:${AWS::AccountId}:log-group:/aws/amplify/*",
+                    },
+                  },
+                  {
+                    Sid: "DescribeLogGroups",
+                    Effect: "Allow",
+                    Action: "logs:DescribeLogGroups",
+                    Resource: {
+                      "Fn::Sub": "arn:aws:logs:${AWS::Region}:${AWS::AccountId}:log-group:*",
+                    },
+                  },
+                ],
+              },
+            },
+          ],
+        },
+      },
+      AmplifyApp: {
+        Type: "AWS::Amplify::App",
+        Properties: {
+          Name: "web-app-${env:STAGE}",
+          Repository: "${env:REPOSITORY}",
+          AccessToken: "${env:GITHUB_TOKEN}",
+          EnableBranchAutoDeletion: true,
+          IAMServiceRole: { "Fn::GetAtt": ["AmplifyAppRole", "Arn"] },
+          Platform: "WEB_COMPUTE",
+          EnvironmentVariables: [
+            {
+              Name: "AMPLIFY_MONOREPO_APP_ROOT",
+              Value: "apps/web",
+            },
+            {
+              Name: "AMPLIFY_DIFF_DEPLOY",
+              Value: "true",
+            },
+            {
+              Name: "AMPLIFY_DIFF_DEPLOY_ROOT",
+              Value: "apps/web",
+            },
+          ],
+        },
+      },
+      AmplifyAppBranch: {
+        Type: "AWS::Amplify::Branch",
+        Properties: {
+          AppId: { Ref: "AmplifyApp" },
+          BranchName: "${env:BRANCH}",
+          EnableAutoBuild: true,
+          EnvironmentVariables: [
+            {
+              Name: "REACT_APP_STAGE",
+              Value: "${env:STAGE}",
+            },
+          ],
+        },
+      },
+      // AmplifyDomain: {
+      //   Type: "AWS::Amplify::Domain",
+      //   Properties: {
+      //     AppId: { Ref: "AmplifyApp" },
+      //   },
+      // },
+    },
+    Outputs: {
+      DomainUrl: {
+        Value: { "Fn::Sub": "https://${AmplifyApp.DefaultDomain}" },
+      },
+      AppId: {
+        Value: { Ref: "AmplifyApp" },
+      },
+    },
+  },
+} satisfies AWS;
